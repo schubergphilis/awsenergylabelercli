29b7ebdcf9467e7e794ac9df06a964857d441a0f
Theodoor Scholte <tscholte@schubergphilis.com>
2021-11-10 17:43:37 +0100

    refactoring

--- awsenergylabelreporter.py
+++ awsenergylabelreporter.py
@@ -1,52 +1,57 @@
 import argparse
-from ExportSecurityHubData import EnergyLabeler
+import json
 import os
-import tempfile
-import boto3
-from urllib.parse import urlparse
-from urllib.parse import urljoin
 import os.path
+import sys
+import tempfile
+from dataclasses import dataclass
+from urllib.parse import urljoin
+from urllib.parse import urlparse
 
+import boto3
+from awsenergylabelerlib import EnergyLabeler
 
 FILENAMES = {
-    energy_label: "energylabel-of-landingzone.json",
-    findings: "securityhub-findings.json",
-    labeled_accounts: "labeled-accounts.json"
+    'energy_label': "energylabel-of-landingzone.json",
+    'findings': "securityhub-findings.json",
+    'labeled_accounts': "labeled-accounts.json"
 }
+
 
 @dataclass
 class ExportConfiguration:
     filename: str
     data: []
 
-class SecurityHubReporter:
 
-    def __init__(self, landing_zone_name, whitelist, blacklist):
+class EnergyLabelReporter:
+
+    def __init__(self, landing_zone_name, allow_list, deny_list):
         self.landing_zone_name = landing_zone_name
-        self.whitelist = whitelist
-        self.blacklist = blacklist
-        self.labeler = EnergyLabeler(landing_zone_name)
+        self.labeler = EnergyLabeler(landing_zone_name, allow_list=allow_list, deny_list=deny_list)
 
         self.export_configurations = [
             ExportConfiguration(FILENAMES['energy_label'],
-                [{'Landing Zone': self.landing_zone_name, 'Landing Zone Energy Label': self.labeler.energy_label_of_landing_zone}]
-            ),
+                                [{'Landing Zone': self.landing_zone_name,
+                                  'Landing Zone Energy Label': self.labeler.energy_label_of_landing_zone}]
+                                ),
             ExportConfiguration(
                 FILENAMES['findings'],
                 [finding.data for finding in self.labeler.self._security_hub.findings]
             ),
             ExportConfiguration(
-                 FILENAMES['labeled_accounts'],
-                 [account.data for account in self.labeler.labeled_accounts]
+                FILENAMES['labeled_accounts'],
+                [account.data for account in self.labeler.labeled_accounts]
             )
-            ]
+        ]
 
     def print_to_console(self):
-        print(f"The Landing Zone named {self.landing_zone_name} has a security score of: {self.labeler.energy_label_of_landing_zone}")
+        print(
+            f"The Landing Zone named {self.landing_zone_name} has a security score of: {self.labeler.energy_label_of_landing_zone}")
 
     def export_as_json_to_fs(self, directory):
-        if(not(path.exists(directory))):
-           os.makedirs(directory)
+        if not (os.path.exists(directory)):
+            os.makedirs(directory)
         for entry in self.export_configurations:
             filepath = os.path.join(directory, entry.filename)
             with open(filepath, 'w') as jsonfile:
@@ -55,7 +60,7 @@ class SecurityHubReporter:
     def export_as_json_to_s3(self, s3_url):
         s3 = boto3.client('s3')
         parsed_url = urlparse.urlparse(s3_url)
-        bucket_name=parsed_url.netloc
+        bucket_name = parsed_url.netloc
         dst_path = parsed_url.path
 
         for entry in self.export_configurations:
@@ -70,25 +75,27 @@ class SecurityHubReporter:
 def is_s3_url(url):
     return urlparse.urlparse(url).scheme == "s3"
 
+
 def is_directory_path(path):
-    parsed_url = urlparse.urlparse(url)
-    return parsed_url.scheme == "" and parsed_url.netloc=="" and len(parsed_url.path) >= 1
+    parsed_url = urlparse.urlparse(path)
+    return parsed_url.scheme == "" and parsed_url.netloc == "" and len(parsed_url.path) >= 1
+
 
 def main():
     parser = argparse.ArgumentParser(description='Reporting tool for AWS Security Hub')
     parser.add_argument('landingzone', type=str, help='The name of the Landing Zone.')
     parser.add_argument('--region', default='eu-west-1', type=str, required=False,
-                         help='The AWS region, default is eu-west-1')
-    parser.add_argument('--whitelist', metavar='AccountId', type=str, nargs='*',
-                         help='A list of AWS Account IDs for which an energy label will be produced.')
-    parser.add_argument('--blacklist', metavar='AccountId', type=str, nargs='*',
-                         help='A list of AWS Account IDs that will be excluded from producing the energy label.')
+                        help='The AWS region, default is eu-west-1')
+    parser.add_argument('--allow-list', metavar='AccountId', type=str, nargs='*',
+                        help='A list of AWS Account IDs for which an energy label will be produced.')
+    parser.add_argument('--deny-list', metavar='AccountId', type=str, nargs='*',
+                        help='A list of AWS Account IDs that will be excluded from producing the energy label.')
     parser.add_argument('--export', default='', type=str, required=False,
-                         help='Exports a snapshot of the reporting data in JSON formatted files to the specified directory or S3 location.')
+                        help='Exports a snapshot of the reporting data in JSON formatted files to the specified directory or S3 location.')
     args = parser.parse_args()
 
-    reporter = SecurityHubReporter(args.landingzone, args.whitelist, args.blacklist)
-    if(args.export):
+    reporter = EnergyLabelReporter(args.landingzone, args.whitelist, args.blacklist)
+    if args.export:
         if is_s3_url(args.export):
             reporter.export_as_json_to_s3(args.export)
         elif is_directory_path(args.export):
@@ -100,7 +107,5 @@ def main():
     return 0
 
 
-
 if __name__ == '__main__':
     sys.exit(main())
-
